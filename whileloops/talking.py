70c157b195224e9dc3efd791f892916f94aad1b7
AmilMasic <amil.masic@gmail.com>
2022-01-03 16:28:09 -0500

    while loops

---
+++ whileloops/talking.py
@@ -0,0 +1,8 @@
+userResponse = input("How is it going? ")
+
+while userResponse != "stop copying me":
+    userResponse = input(f"{userResponse}\n")
+
+# while userResponse != "stop copying me":
+#     print(userResponse)
+#     userResponse = input()
---
+++ whileloops/while.py
@@ -0,0 +1,6 @@
+msg = input("Whats the secret password?")
+
+while msg != "bananas":
+    print("WRONG")
+    msg = input("Whats the secret password?")
+print("Correct")
