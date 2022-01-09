522a9e19ffcffde39b14415cd86cef1bdb91a759
Amil Masic <amil.masic@gmail.com>
2022-01-09 11:06:58 -0500

    class methods

--- OOP/user.py
+++ OOP/user.py
@@ -1,6 +1,14 @@
 class User:
     active_users = 0
 
+    @classmethod
+    def display_active_users(cls):
+        return f"There are currently {cls.active_users} users"
+    @classmethod
+    def from_string(cls, data_str):
+        first, last, age = data_str.split(",")
+        return cls(first, last, age)
+
     def __init__(self, first, last, age):
         self.first = first
         self.last = last
@@ -20,10 +28,11 @@ class User:
 user1 = User("Joe", "Smith", 68)
 user2 = User("Blanca", "Lopez", 41)
 
-print(user1.first, user1.last)
+# print(user1.first, user1.last)
 
-print(user2.full_name())
-print(user2.initials())
-print(User.active_users)
-print(user2.logout())
-print(User.active_users)
+# print(user2.full_name())
+# print(user2.initials())
+# print(User.active_users)
+# print(User.display_active_users())
+tom = User.from_string("Tom,Jones, 69")
+print(tom.full_name())
