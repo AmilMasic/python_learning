bef55afd2b5e93d22459688af2f37a9abf9a0ff5
Amil Masic <amil.masic@gmail.com>
2022-01-09 10:55:31 -0500

    class variables and how to call them

--- OOP/user.py
+++ OOP/user.py
@@ -1,11 +1,29 @@
 class User:
+    active_users = 0
+
     def __init__(self, first, last, age):
         self.first = first
         self.last = last
         self.age = age
+        User.active_users += 1
+
+    def full_name(self):
+        return f"{self.first} {self.last}"
+
+    def initials(self):
+        return f"{self.first[0]}.{self.last[0]}."
+
+    def logout(self):
+        User.active_users -= 1
+        return f"{self.first} has logged out"
 
 user1 = User("Joe", "Smith", 68)
 user2 = User("Blanca", "Lopez", 41)
 
 print(user1.first, user1.last)
-print(user2.first, user2.last)
+
+print(user2.full_name())
+print(user2.initials())
+print(User.active_users)
+print(user2.logout())
+print(User.active_users)
